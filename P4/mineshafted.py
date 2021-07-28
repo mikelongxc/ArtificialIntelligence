4afa29463713b52cff821e003d9886867b60f64b
Michael Long <mikelongxc@gmail.com>
2021-07-28 16:57:43 -0700

    init P4

---
+++ P4/mineshafted.py
@@ -0,0 +1,102 @@
+# Name:         
+# Course:       CSC 480
+# Instructor:   Daniel Kauffman
+# Assignment:   Mine Shafted
+# Term:         Summer 2021
+
+import itertools
+from typing import Callable, Generator, List, Tuple
+
+
+class BoardManager:  # do not modify
+
+    def __init__(self, board: List[List[int]]):
+        """
+        An instance of BoardManager has two attributes.
+
+            size: A 2-tuple containing the number of rows and columns,
+                  respectively, in the game board.
+            move: A callable that takes an integer as its only argument to be
+                  used as the index to explore on the board. If the value at
+                  that index is a clue (non-mine), this clue is returned;
+                  otherwise, an error is raised.
+
+        This constructor should only be called once per game.
+
+        >>> board = [[0, 1, 1], [0, 2, -1], [0, 2, -1], [0, 1, 1]]
+        >>> bm = BoardManager(board)
+        >>> bm.size
+        (4, 3)
+        >>> bm.move(4)
+        2
+        >>> bm.move(5)
+        Traceback (most recent call last):
+        ...
+        RuntimeError
+        """
+        self.size: Tuple[int, int] = (len(board), len(board[0]))
+        it: Generator[int, int, None] = BoardManager._move(board, self.size[1])
+        next(it)
+        self.move: Callable[[int], int] = it.send
+
+    def get_adjacent(self, index: int) -> List[int]:
+        """
+        Return a list of indices adjacent (including diagonally) to the given
+        index. All adjacent indices are returned, regardless of whether or not
+        they have been explored.
+
+        >>> board = [[0, 1, 1], [0, 2, -1], [0, 2, -1], [0, 1, 1]]
+        >>> bm = BoardManager(board)
+        >>> bm.get_adjacent(3)
+        [0, 1, 4, 6, 7]
+        """
+        row, col = index // self.size[1], index % self.size[1]
+        return [i * self.size[1] + j
+                for i in range(max(row - 1, 0), min(row + 2, self.size[0]))
+                for j in range(max(col - 1, 0), min(col + 2, self.size[1]))
+                if index != i * self.size[1] + j]
+
+    @staticmethod
+    def _move(board: List[List[int]], width: int) -> Generator[int, int, None]:
+        """
+        A generator that may be sent integers (indices to explore on the board)
+        and yields integers (clues for the explored indices).
+
+        Do not call this method directly; instead, call the |move| instance
+        attribute, which sends its index argument to this generator.
+        """
+        index = (yield 0)
+        while True:
+            clue = board[index // width][index % width]
+            if clue == -1:
+                raise RuntimeError
+            index = (yield clue)
+
+
+
+
+def sweep_mines(bm: BoardManager) -> List[List[int]]:
+    """
+    Given a BoardManager (bm) instance, return a solved board (represented as a
+    2D list) by repeatedly calling bm.move(index) until all safe indices have
+    been explored. If at any time a move is attempted on a non-safe index, the
+    BoardManager will raise an error; this error signifies the end of the game
+    and should not attempt to be caught.
+
+    >>> board = [[0, 1, 1], [0, 2, -1], [0, 2, -1], [0, 1, 1]]
+    >>> bm = BoardManager(board)
+    >>> sweep_mines(bm)
+    [[0, 1, 1], [0, 2, -1], [0, 2, -1], [0, 1, 1]]
+    """
+
+
+
+
+def main() -> None:  # optional driver
+    board = [[0, 1, 1], [0, 2, -1], [0, 2, -1], [0, 1, 1]]
+    bm = BoardManager(board)
+    assert sweep_mines(bm) == board
+
+
+if __name__ == "__main__":
+    main()
