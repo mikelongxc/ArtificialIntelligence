a416736b32eaccb063d0019366ece6f9af70cb8d
Michael Long <mikelongxc@gmail.com>
2021-08-08 19:47:22 -0700

    c = 1

--- P5/boardstupid.py
+++ P5/boardstupid.py
@@ -213,7 +213,7 @@ class StateNode:
         self.n = 0
         self.t = 0
 
-        self.c = 100 ** 0.5
+        self.c = 1
 
     def get_ucb(self) -> float:
         if self.t == 0 or self.n == 0:
